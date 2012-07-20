5531b08302e10f98b404b811822a141460240af1
contacts.kim@gmail.com <contacts.kim@gmail.com@626b9d6f-df83-957e-9f3e-fab6558fbc13>
2012-07-20 12:23:18 +0000

    importing libs

---
+++ trunk/discussions/ExitGames-Photon-Server-SDK_v3-0-24-3243-RC9/src-server/Loadbalancing/LoadBalancing/Events/GameServerOfflineEvent.cs
@@ -0,0 +1,16 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="GameServerOfflineEvent.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Photon.LoadBalancing.Events
+{
+    using Photon.SocketServer.Rpc;
+
+    public class GameServerOfflineEvent : DataContract
+    {
+        [DataMember(Code = 0, IsOptional=true)]
+        public int TimeLeft { get; set; }
+    }
+}
