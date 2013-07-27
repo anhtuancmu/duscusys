839f65fe807b403a6a3230f60ef75be4cb1ee195
contacts.kim <contacts.kim@626b9d6f-df83-957e-9f3e-fab6558fbc13>
2013-07-27 11:51:33 +0000

    -cloud storage viewer: removed double tap to view a file -cloud storage viewer: add view button to view a file

---
+++ branches/WindowBranch/discussions/CloudStorage/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace CloudStorage.Converters
+{
+    [ValueConversion(typeof (string), typeof (string))]
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if ((bool) value)
+                return Visibility.Visible;
+            
+            return Visibility.Collapsed;            
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+                                  System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
--- branches/WindowBranch/discussions/CloudStorage/Model/FileEntry.cs
+++ branches/WindowBranch/discussions/CloudStorage/Model/FileEntry.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows.Media;
 using DropNet.Models;
+using System.Linq;
 
 namespace CloudStorage.Model
 {
@@ -11,16 +12,18 @@ namespace CloudStorage.Model
         public ImageSource Image { get; set; }
         public string IdString { get; set; }
         public bool IsFolder { get; set; }
+        public bool IsViewable { get; set; }
         public string GdocWebUrl { get; set; }
         public bool IsGDrive { get; set; } //true iif it's gdrive file
 
-        public FileEntry(DateTime modified, string title, ImageSource img, string idString, bool isFolder)
+        public FileEntry(DateTime modified, string title, ImageSource img, string idString, bool isFolder, bool isViewable)
         {
             Modified = modified;
             Title = title;
             Image = img;
             IdString = idString;
             IsFolder = isFolder;
+            IsViewable = isViewable;
         }
 
         public FileEntry(MetaData md, ImageSource thumb)
@@ -31,6 +34,7 @@ namespace CloudStorage.Model
             IdString = md.Path;
             IsFolder = md.Is_Dir;
             IsGDrive = false;
+            IsViewable = !md.Is_Dir && IsExtViewable(md.Extension.Replace(".", ""));
         }
 
         public FileEntry(Google.Apis.Drive.v2.Data.File file, ImageSource thumb, string gdocWebUrl)
@@ -41,7 +45,38 @@ namespace CloudStorage.Model
             IdString = file.Id;
             IsFolder = GDriveStorage.IsFolder(file);
             GdocWebUrl = gdocWebUrl;
-            IsGDrive = true;
+            IsGDrive = true;            
+            IsViewable = !IsFolder && (IsExtViewable(file.FileExtension) || 
+                                       !string.IsNullOrWhiteSpace(GdocWebUrl));
+        }
+
+        static readonly string[] _images = new string[] { "jpg", "jpeg", "bmp", "png" };
+        static readonly string[] _word = new string[] { "docx", "docm", "dotx", "dotm", ".doc", "rtf", "odt" };
+        static readonly string[] _excel = new string[] { "xlsx", "xlsm", "xlsb", "xltx", ".xltm", "xls", "xlt" };
+        static readonly string[] _powerPoint = new string[] { "pptx", "ppt", "pptm", "ppsx", ".pps", "potx", "pot", "potm", "odp" };
+        public static bool IsExtViewable(string ext)
+        {
+            if (ext == null)
+                return false;
+
+            ext = ext.ToLower();
+
+            if (ext == "txt")
+                return true;
+
+            if (_images.Contains(ext))
+                return true;
+
+            if (_word.Contains(ext))
+                return true;
+
+            if (_excel.Contains(ext))
+                return true;
+
+            if (_powerPoint.Contains(ext))
+                return true;
+
+            return false;
         }
     }
 }
