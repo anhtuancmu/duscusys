a8d6224480eb7e7675089f4310d732728b6feb73
contacts.kim <contacts.kim@626b9d6f-df83-957e-9f3e-fab6558fbc13>
2013-07-30 07:33:59 +0000

    -cloud storage: change FileEntry event handlers from preview event to normal event

--- discussions/LoginEngine/Properties/AssemblyInfo.cs
+++ discussions/LoginEngine/Properties/AssemblyInfo.cs
@@ -54,5 +54,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.0.13.8")]
-[assembly: AssemblyFileVersion("0.0.13.8")]
+[assembly: AssemblyVersion("0.0.13.9")]
+[assembly: AssemblyFileVersion("0.0.13.9")]
