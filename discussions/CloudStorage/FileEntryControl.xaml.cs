c86c9a480b2e419d62c4345938c57e7f42f6dbd6
contacts.kim <contacts.kim@626b9d6f-df83-957e-9f3e-fab6558fbc13>
2013-07-31 07:20:37 +0000

    -remove any special handling of file selection in cloud storage (win8 touch selection issue)

--- discussions/CloudStorage/FileEntryControl.xaml.cs
+++ discussions/CloudStorage/FileEntryControl.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
 
 namespace CloudStorage
 {
@@ -15,24 +14,9 @@ namespace CloudStorage
             remove { RemoveHandler(RequestViewEvent, value); }
         }
 
-        public static readonly RoutedEvent CustSelectionEvent = EventManager.RegisterRoutedEvent(
-            "CustSelection", RoutingStrategy.Bubble, typeof (RoutedEventHandler), typeof (FileEntryControl));
-
-        public event RoutedEventHandler CustSelection
-        {
-            add { AddHandler(CustSelectionEvent, value); }
-            remove { RemoveHandler(CustSelectionEvent, value); }
-        }
-
         public FileEntryControl()
         {
             InitializeComponent();
-        }
-
-        private void OnSingleClick(object sender, InputEventArgs e)
-        {
-            RaiseEvent(new RoutedEventArgs(CustSelectionEvent));
-            e.Handled = true;
         }
 
         private void BtnView_OnClick(object sender, RoutedEventArgs e)
