059e3521213a24bf25a0d7f8015563af25c2674e
contacts.kim <contacts.kim@gmail.com>
2013-11-16 10:24:47 +0000

    -fix lost comments

--- discussions/ctx/PrivateCenterCtx.cs
+++ discussions/ctx/PrivateCenterCtx.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Controls.Primitives;
+using Discussions.DbModel;
 using Discussions.rt;
 using Discussions.stats;
 
@@ -25,13 +33,48 @@ namespace Discussions.ctx
 
         public static void SaveChangesIgnoreConflicts()
         {
-            try
-            {
-                Get().SaveChanges();
-            }
-            catch (Exception)
-            {
-            }
+            int nAttempts = 0;
+            bool saveFailed;    
+            do   
+            {        
+                saveFailed = false;
+                try
+                {
+                    Get().SaveChanges();
+                }
+                catch (System.Data.Entity.Core.OptimisticConcurrencyException ex)        
+                {           
+                    saveFailed = true;            
+
+                    Comment[] addedConflictedComments =
+                        ex.StateEntries.Where(se => se.State == EntityState.Modified && (se.Entity as Comment)!=null)
+                            .Select(se => se.Entity)
+                            .Cast<Comment>()
+                            .ToArray();
+
+                    ArgPoint conflictedArgPoint = null;
+
+                    //detach conflicted comments to prevent their modification by the Refresh() call
+                    foreach (var addedConflictedComment in addedConflictedComments)
+                    {
+                        conflictedArgPoint = addedConflictedComment.ArgPoint;
+                        Get().Detach(addedConflictedComment);
+                    }
+
+                    if (conflictedArgPoint != null)
+                    {
+                        Get().Refresh(RefreshMode.StoreWins, conflictedArgPoint);
+
+                        foreach (var comment in addedConflictedComments)
+                        {
+                            Get().AddToComment(comment);
+                        }
+
+                        DaoUtils.RemovePlaceholders(conflictedArgPoint);
+                        DaoUtils.EnsureCommentPlaceholderExists(conflictedArgPoint);
+                    }
+                }
+            } while (saveFailed && ++nAttempts < 6);
         }
 
         public static void DropContext()
