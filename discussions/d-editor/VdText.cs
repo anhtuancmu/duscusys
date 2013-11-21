762b3ed11d91341ca765e7a33093e130242762e0
contacts.kim <contacts.kim@gmail.com>
2013-11-21 10:40:09 +0000

    Make Desktop Textbox Enter able to bring the text to next line

--- discussions/d-editor/VdText.cs
+++ discussions/d-editor/VdText.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows.Controls;
 using System.Windows;
-using System.Windows.Shapes;
 using System.Windows.Input;
 using System.Windows.Media;
 using Discussions;
