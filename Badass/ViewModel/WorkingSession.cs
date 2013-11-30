f6b86fde50d24195480f7499afd148ba730fe697
contacts.kim <contacts.kim@gmail.com>
2013-11-30 08:17:01 +0000

    -ui tweaks

---
+++ Badass/ViewModel/WorkingSession.cs
@@ -0,0 +1,13 @@
+﻿using Badass.Model;
+
+namespace Badass.ViewModel
+{
+    public static class WorkingSession
+    {
+        public static Project LoadedProject
+        {
+            get;
+            set;
+        }
+    }
+}
