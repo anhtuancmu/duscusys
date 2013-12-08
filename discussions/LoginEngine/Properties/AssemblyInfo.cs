7d59a93b091dc0a6b907c34101bcf523ce2f2b34
contacts.kim <contacts.kim@gmail.com>
2013-12-08 09:15:42 +0000

    -16.0

--- discussions/LoginEngine/Properties/AssemblyInfo.cs
+++ discussions/LoginEngine/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.0.15.9")]
-[assembly: AssemblyFileVersion("0.0.15.9")]
+[assembly: AssemblyVersion("0.0.16.0")]
+[assembly: AssemblyFileVersion("0.0.16.0")]
--- discussions/d-editor/InkPalette.xaml.cs
+++ discussions/d-editor/InkPalette.xaml.cs
@@ -1,16 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Microsoft.Surface.Presentation.Controls;
 using Microsoft.Surface.Presentation.Controls.Primitives;
 
@@ -57,6 +48,11 @@ namespace DistributedEditor
                 radErase.IsChecked = false;
             else
                 radInk.IsChecked = false;
+        }
+
+        public void OnDrawingStarted()
+        {
+            b_8.IsChecked = true;
         }
 
         private void btnFinishDrawing_Click_1(object sender, RoutedEventArgs e)
