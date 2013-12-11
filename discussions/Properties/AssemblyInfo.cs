82d5b4035281798cd179b5d925f43134e1843d58
clauxz <clauxz@626b9d6f-df83-957e-9f3e-fab6558fbc13>
2013-12-11 11:59:42 +0000

    minor translation  in ink palette .. login engine .. 2013 copy right  windows 7 surface

--- discussions/Properties/AssemblyInfo.cs
+++ discussions/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tohoku University")]
 [assembly: AssemblyProduct("Discussions")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
