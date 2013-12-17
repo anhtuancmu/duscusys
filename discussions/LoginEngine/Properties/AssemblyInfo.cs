8425a78c8b8688bf6f5c6d7d14dc77f5fef0b4d7
contacts.kim <contacts.kim@gmail.com>
2013-12-17 12:59:57 +0000

    -hide browser until page loads completely -change layout of LargeBadgeView

--- discussions/LoginEngine/Properties/AssemblyInfo.cs
+++ discussions/LoginEngine/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.0.16.8")]
-[assembly: AssemblyFileVersion("0.0.16.8")]
+[assembly: AssemblyVersion("0.0.16.9")]
+[assembly: AssemblyFileVersion("0.0.16.9")]
