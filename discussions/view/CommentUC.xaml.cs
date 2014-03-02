6ba1a4320afde96f3aab9ed9cffc211dcc5a9636
contacts.kim <contacts.kim@gmail.com>
2014-03-02 16:45:39 +0000

    -latest issues

--- discussions/view/CommentUC.xaml.cs
+++ discussions/view/CommentUC.xaml.cs
@@ -99,7 +99,7 @@ namespace Discussions.view
 
         static readonly Regex HyperlinkValidator =
             new Regex(
-                @"^(ht|f)tp(s?)\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-\.\?\,\'\/\\\+&amp;%\$#_]*)?$");
+                @"^(ht|f)tp(s?)\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([=a-zA-Z0-9\-\.\?\,\'\/\\\+&amp;%\$#_]*)?$");
 
         public CommentUC()
         {
